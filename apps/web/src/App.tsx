fb79bddc79a6675387df9a081f6b11148eb27dc8
reckai <casa101565@gmail.com>
2025-12-26 23:54:40 +0200

    feat: delete context

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,18 +1,13 @@
-import { clearStack, context } from '@reatom/core'
-import { reatomContext } from '@reatom/react'
 import { RouterProvider } from 'react-router-dom'
-// import { initRoomEffect } from '@/models/room.model'
+
 import { router } from '@/router'
 import './i18n'
 
-clearStack()
-const rootFrame = context.start()
-
 function App() {
   return (
-    <reatomContext.Provider value={rootFrame}>
-      <RouterProvider router={router} />
-    </reatomContext.Provider>
+
+    <RouterProvider router={router} />
+
   )
 }
 
