bd6a05d3c1becbee626cb9b388c60df8bb712a06
reckai <casa101565@gmail.com>
2025-12-31 14:13:11 +0200

    fix: change buildkit railway

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN corepack enable
 FROM base AS builder
 WORKDIR /app
 COPY . .
-RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
+RUN pnpm install --frozen-lockfile
 
 # Generate Prisma Client
 RUN pnpm run db:generate
---
+++ apps/web/src/pages/Room/AnimeSelect/animeSelect.model.ts
@@ -0,0 +1,13 @@
+// import { roomDataAtom } from "@/models/room.model";
+// import { effect } from "@reatom/core";
+// import axios from 'axios';
+// import { wrap } from "module";
+// const asd = axios.create({
+//     baseURL:process.env.BACKAND_URL || 'http://localhost:4000/api'
+// })
+
+// // effect(async()=>{
+// //     const roomData = roomDataAtom();
+// //     // if( roomData?.status !== 'SELECTING')return;
+// //     const response = await wrap(asd.post('/batch'))
+// // },'fetchAnimeEffect')
